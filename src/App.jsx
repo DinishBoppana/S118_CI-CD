f66419c4af69031945588cb7bc9c9876a699096b
GirishBoppana <girishboppana4419@gmail.com>
2025-07-29 22:29:29 +0530

    “V1.0.1”

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S118_CI/CD">
+    <Router basename="/S118_CI-CD">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
